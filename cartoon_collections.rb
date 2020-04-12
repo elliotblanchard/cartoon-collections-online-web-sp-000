ce21a6af6e1be75be58a2a0d24ae52ff7fa45c7e
Elliot Blanchard <public@elliotblanchard.com>
2020-04-12 19:30:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def long_planeteer_calls(calls) # code an argument here
   calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(items)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  items.detect{|item| cheese_types.include(item)}
 end
